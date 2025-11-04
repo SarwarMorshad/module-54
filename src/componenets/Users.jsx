30ec40ed468c39f55e4ee1f8a90fe2afafc9d206
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-11-04 11:26:56 +0100

    Data saved to DB

--- src/componenets/Users.jsx
+++ src/componenets/Users.jsx
@@ -1,11 +1,38 @@
 import React from "react";
 
 const Users = () => {
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+    console.log(user);
+
+    // send data to server
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Data saved is successful", data);
+        if (data.insertedId) {
+          alert("User added successfully");
+          event.target.reset();
+        }
+      });
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleAddUser}>
+        <label htmlFor="name">Enter Your Name: </label>
         <input type="text" name="name" />
         <br />
+        <label htmlFor="email">Enter Your Email: </label>
         <input type="email" name="email" />
         <br />
         <input type="submit" value="Add User" />
